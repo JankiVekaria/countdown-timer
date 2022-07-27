6cdf4ac59a665765f0eadb6f13a6b337fffb4925
janki <vekariajanki@gmail.com>
2022-07-27 16:54:48 -0400

    countdown

---
+++ script.js
@@ -0,0 +1,32 @@
+daysEl = document.getElementById('days');
+hoursEl = document.getElementById('hours');
+minsEl = document.getElementById('mins');
+secondsEl = document.getElementById('seconds');
+
+
+const newYears = "1 Jan 2023";
+
+function countdown(){
+	const newYearsDate = new Date(newYears);
+	const currentDate = new Date();
+	
+	const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+	const days = Math.floor(totalSeconds / 3600 / 24);
+	const hours = Math.floor(totalSeconds / 3600) %24;
+	const mins = Math.floor(totalSeconds / 60) %60;
+	const seconds = Math.floor(totalSeconds) % 60;
+	
+	daysEl.innerHTML = days;
+	hoursEl.innerHTML = hours;
+	minsEl.innerHTML = mins;
+	secondsEl.innerHTML = seconds;
+	
+}
+
+
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
